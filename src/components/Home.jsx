a0f2e16dd51feabd3970b20639bc4877e27643d2
maulik7984335855 <maulikprajapati722004@gmail.com>
2025-02-10 21:13:34 +0530

    changing some style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,9 @@ const Home = () => {
               value={selectedOption}
               className="w-[320px] rounded px-2 py-3 mt-2"
             >
-              <option className="text-black" value="">All</option>
+              <option className="text-black" value="">
+                All
+              </option>
               {material.map((data) => (
                 <>
                   <option
@@ -37,36 +39,38 @@ const Home = () => {
 
         {/* Display PDF files */}
         <div className="main flex flex-wrap justify-center gap-4 mt-4">
-          {filtered.length > 0 ? filtered.map((data) => (
-            <div
-              key={data._id}
-              className="border rounded-lg p-4 w-[230px] shadow-lg"
-            >
-              <h3 className="text-lg font-semibold">{data.subjectName}</h3>
-              <p className="text-gray-600">{data.description}</p>
+          {filtered.length > 0 ? (
+            filtered.map((data) => (
+              <div
+                key={data._id}
+                className="border rounded-lg p-4 w-[230px] shadow-lg"
+              >
+                <h3 className="text-lg font-semibold">{data.subjectName}</h3>
+                <p className="text-gray-600">{data.description}</p>
 
-              {/* Display PDF */}
-              <div className="mt-3">
-                <iframe
-                  src={data.subjectMaterial}
-                  width="200px"
-                  height="200px"
-                  className="border "
-                ></iframe>
-              </div>
+                {/* Display PDF */}
+                <div className="mt-3">
+                  <iframe
+                    src={data.subjectMaterial}
+                    className="border w-[200px] h-[250px] object-contain "
+                  />
+                </div>
 
-              {/* Download Button */}
-              <div className="mt-4 text-center">
-                <a
-                  href={data.subjectMaterial}
-                  download
-                  className="bg-blue-500 text-white px-3 py-2 rounded"
-                >
-                  Download PDF
-                </a>
+                {/* Download Button */}
+                <div className="mt-4 text-center">
+                  <a
+                    href={data.subjectMaterial}
+                    download
+                    className="bg-blue-500 text-white px-3 py-2 rounded"
+                  >
+                    Download PDF
+                  </a>
+                </div>
               </div>
-            </div>
-          )) : <h2 className="font-bold text-xl">Not Found</h2>}
+            ))
+          ) : (
+            <h2 className="font-bold text-xl">Not Found</h2>
+          )}
         </div>
       </div>
     </>
